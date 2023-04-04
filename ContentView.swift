612a5d43b26f571347d85404705611e7cf0c07a3
Victor Chandra <41635105+mzmznasipadang@users.noreply.github.com>
2023-04-04 16:22:33 +0700

    Updated Nav Bar

--- ContentView.swift
+++ ContentView.swift
@@ -2,26 +2,50 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var isSplashShowing = true
+    @State private var selectedTab: Int = 0
     
     var body: some View {
         ZStack {
-            TabView {
+            TabView(selection: $selectedTab) {
                 HomeView()
                     .tabItem {
-                        Label("Home", systemImage: "house")
+                        if selectedTab == 1{
+                            Label("Home", systemImage: "house")
+                        } else {
+                            Label("Home", systemImage: "house.fill")
+                        }
                     }
+                    .tag(0)
+
                 QuizView()
                     .tabItem {
-                        Label("Quiz", systemImage: "book")
+                        if selectedTab == 2 {
+                            Label("Quiz", systemImage: "book.fill")
+                        } else {
+                            Label("Quiz", systemImage: "text.book.closed.fill")
+                        }
                     }
+                    .tag(2)
+                
                 TopicsView()
                     .tabItem {
-                        Label("Topics", systemImage: "message.badge.filled.fill")
+                        if selectedTab == 3{
+                            Label("Topics", systemImage: "checkmark.message.fill")
+                        } else {
+                            Label("Topics", systemImage: "message.badge.filled.fill")
+                        }
                     }
+                    .tag(3)
+
                 SettingsView()
                     .tabItem {
-                        Label("Settings", systemImage: "gear.circle.fill")
-                    }
+                    if selectedTab == 4{
+                    Label("Settings", systemImage: "gear.badge")
+                    } else {
+                    Label("Settings", systemImage: "gear")
+                            }
+                        }
+                    .tag(4)
             }
             
             SplashView(isShowing: $isSplashShowing)
